b3a1b3ef98375822eef0819f6a6f814941fbd807
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-23 22:55:27 +0300

    Описаны роутинги (пока на компоненты-заглушки)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store } from 'app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
+import {Login} from 'features/Login';
+import {Register} from 'features/Register';
+import {Profile} from 'features/Profile';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/check-email",
+    element: <h1>Check email</h1>,
+  },
+  {
+    path: "/set-new-password",
+    element: <h1>Set new password</h1>,
+  },
+  {
+    path: "/forgot-password",
+    element: <h1>Forgot password</h1>,
+  },
+  {
+    path: "/profile",
+    element: <Profile/>,
+  },
+  {
+    path: "/packs",
+    element: <h1>Packs</h1>,
+  },
+  {
+    path: "/cards",
+    element: <h1>Cards</h1>,
+  },
+  {
+    path: "/learn",
+    element: <h1>Learn</h1>,
+  },
+]);
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,6 +62,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <RouterProvider router={router} />
       <App />
     </Provider>
   </React.StrictMode>
