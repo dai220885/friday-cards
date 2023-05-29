9836173c3d11228835e2c50b8fd1a91eaa3ca410
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-30 00:59:00 +0300

    компонента логинизации, диспатчит санку authThunks.login занятие 17 (пятница 01)

---
+++ src/features/login/Login.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {useAppDispatch, useAppSelector} from 'app/hooks';
+import {authActions, authThunks} from 'features/auth/auth.slice';
+import s from 'features/register/register.module.css';
+
+export const Login = () => {
+	const isLoading = useAppSelector((state) => state.auth.isLoading);
+	const dispatch = useAppDispatch();
+
+	const loginHandler = () => {
+		dispatch(authThunks.login({email: 'dylevich220885.2@gmail.com', password: '12345678', rememberMe: true}))
+	};
+
+	return (
+		<div className={s.container}>
+			{isLoading && <h1>Loader...</h1>}
+			<h1>Login</h1>
+			<button onClick={loginHandler}>login</button>
+		</div>
+	);
+};
