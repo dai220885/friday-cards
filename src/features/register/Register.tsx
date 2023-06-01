fe76bfbbb446f1cc60512421d7824f190f230487
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-01 22:49:04 +0300

    Реализовал переадресацию на форму логинизации, если регистрация прошла успешно (если isRegistered===true) занятие 17 (пятница 01 домашка)

--- src/features/register/Register.tsx
+++ src/features/register/Register.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import s from './register.module.css'
-import {useAppDispatch} from 'app/hooks';
+import {useAppDispatch, useAppSelector} from 'app/hooks';
 import {authThunks} from 'features/auth/auth.slice';
+import {Navigate} from 'react-router-dom';
 
 export const Register = () => {
 	const dispatch = useAppDispatch();
+	const isRegistered = useAppSelector((state)=>state.auth.isRegistered)
 
 	const registerHandler = () => {
-		dispatch(authThunks.register({email: 'dylevich220885@gmail.com', password: '12345678'}));
+		dispatch(authThunks.register({email: 'dylevich220885.4@gmail.com', password: '12345678'}));
 	};
 
-	return (
-		<div className={s.container}>
-			<h1>Register</h1>
-			<button onClick={registerHandler}>register</button>
-		</div>
+	return (!isRegistered
+			? <div className={s.container}>
+		<h1>Register</h1>
+		<button onClick={registerHandler}>register</button>
+	</div>
+			: <Navigate to={'/login'}/>
+
 	);
 };
