8f6c2344f629fb04180da71dbee280c91cacf3c9
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-03 22:49:40 +0300

    добавлен addMatcher в extraReducers занятие 18 (пятница 02)

--- src/features/App/app.slice.ts
+++ src/features/App/app.slice.ts
@@ -1,4 +1,6 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {isAxiosError} from 'axios';
+import {toast} from 'react-toastify';
 
 // Создаем reducer с помощью slice
 const slice = createSlice({
@@ -21,9 +23,76 @@ const slice = createSlice({
 		setIsAppInitialized : (state, action: PayloadAction<{ isAppInitialized: boolean}>) =>{
 			state.isAppInitialized = action.payload.isAppInitialized
 		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addMatcher( (action)=>{
+			//проверяем, если диспатчится санка и ее состояние pending, то выполняем нужную нам логику
+			return action.type.endsWith('/pending');
+		}, (state, action)=> {
+			state.isLoading = true
+		})
+			.addMatcher( (action)=>{
+				//проверяем, если диспатчится санка и ее состояние fulfilled, то выполняем нужную нам логику
+				return action.type.endsWith('/fulfilled');
+			}, (state, action)=> {
+				state.isLoading = false
+			})
+			.addMatcher(
+				(action) => {
+					return action.type.endsWith("/rejected")
+				},
+				(state, { payload: { error } }) => {
+					//если состояни санки rejected, то показываем ошибку
+					state.isLoading = false
+					const errorMessage = getErrorMessage(error)
+					if (errorMessage === null) return
+					//console.log(`проверка: ${errorMessage}`)
+					toast.error(errorMessage)
+				},)
 
+			// .addMatcher( (action)=>{
+			// 	//проверяем, если диспатчится санка и ее состояние pending, то выполняем нужную нам логику
+			// 	return action.type.endsWith('/rejected');
+			// }, (state, action)=> {
+			// 	const {error, showGlobalError = true} = action.paylaad
+			// 	state.isLoading = false
+			// 	if(!showGlobalError) return
+			// 	let errorMessage = ''
+			// 	if (isAxiosError(error)) {
+			// 		errorMessage = error?.response?.data?.error ?? error.message // оператор ?? похож на ||, но "псевдоложь" будет только null или undefined
+			// 	} //else if(e instanceof Object && 'message' in e) {
+			// 	else if(error instanceof Error) {
+			// 		errorMessage = `Native error: ${error.message}`
+			// 	}
+			// 	else errorMessage = JSON.stringify(error)
+			// 	toast.error(errorMessage)
+			//
+			// })
 	},
 })
+
+
+/* if null is returned no message should be shown */
+function getErrorMessage(error: unknown): null | string {
+	//debugger
+	if (isAxiosError(error)) {
+		//если не хотим показывать, что именно за ошибка при логинизации
+		if (
+			error?.response?.status === 400 &&
+			error?.request.responseURL.endsWith("/login")
+		) {
+			//return null
+			return 'incorrect login or password'
+		}
+		return error?.response?.data?.error ?? error.message
+	}
+	if (error instanceof Error) {
+		return `Native error: ${error.message}`
+	}
+	return JSON.stringify(error)
+}
+
 
 export const appReducer = slice.reducer
 export const appActions = slice.actions
