b3c8da3f961711622b25a7639bc70a23f6637a7e
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-06 00:16:29 +0300

    добавлены запросы me, logout, updateProfileData занятие 18 (пятница 02)

--- src/features/auth/auth.api.ts
+++ src/features/auth/auth.api.ts
@@ -2,16 +2,25 @@ import {AuthInstance} from 'features/auth/auth.instance';
 import axios from 'axios';
 
 export const AuthApi = {
+	me: () => {
+		return AuthInstance.post<UserType>("me")
+	},
 	register: (params: RegisterArgsType) => {
 		return AuthInstance.post<RegisterResponseType>('register', params)
 	},
 	login: (params: LoginArgsType) => {
-		return AuthInstance.post<UserType>("login", params)
+		return AuthInstance.post<UserType>('login', params)
+	},
+	logout: () => {
+		return AuthInstance.delete('me')
 	},
 	forgotPassword: (arg: ForgotPassArgsType) => {
-		return AuthInstance.post("forgot", arg)
+		return AuthInstance.post('forgot', arg)
 		//return axios.post("https://neko-back.herokuapp.com/2.0/auth/forgot", arg, {withCredentials: true})
 	},
+	updateProfileData: (arg: PartialUserType) => {
+		return AuthInstance.put<UserType>('me', arg)
+	}
 }
 
 //types:
@@ -27,11 +36,11 @@ type FieldsToForgot = {
 	from?: string
 	message: string
 }
-export type ForgotPassArgsType = Pick<UserType, "email"> & FieldsToForgot
-export type AddedUserType = Omit<UserType, "token" | "tokenDeathTime">
-export type RegisterArgsType = Pick<UserType, "email"> & PasswordToPick
-export type LoginArgsType = Pick<UserType, "email" | "rememberMe"> & PasswordToPick
-export type PartialUserType = Partial<UserType>
+export type ForgotPassArgsType = Pick<UserType, 'email'> & FieldsToForgot
+export type AddedUserType = Omit<UserType, 'token' | 'tokenDeathTime'>
+export type RegisterArgsType = Pick<UserType, 'email'> & PasswordToPick
+export type LoginArgsType = Pick<UserType, 'email' | 'rememberMe'> & PasswordToPick
+export type PartialUserType = Partial<UserType> //пока используется для updateUser и на сервер отправляетя только новое имя
 //Omit - убирает из типа указанные поля,
 // Pick - выбирает из указанного типа ТОЛЬКО указанные поля,
 // Partial - берет все поля из указанного типа, но делает их не обязательными.
